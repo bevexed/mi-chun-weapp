7530d55d1564987be81f7026d319b0fca546595d
bevexed <547551650@qq.com>
2019-06-06 09:50:15 +0800

    详情页面 按钮

--- src/pages/good-detail/good-detail.vue
+++ src/pages/good-detail/good-detail.vue
@@ -1,6 +1,11 @@
 <template>
 	<div class="good-detail">
-		<image mode="widthFix" class="shop-img" src="../../static/dev/1.jpg"></image>
+		<image class="shop-img" mode="widthFix" src="../../static/dev/1.jpg"></image>
+
+		<footer>
+			<div class="custom">客服</div>
+			<div class="buy">立即购买</div>
+		</footer>
 	</div>
 </template>
 
@@ -18,6 +23,34 @@
 	.good-detail {
 		.shop-img {
 			width: 100%;
+		}
+
+		footer {
+			$height: upx(98);
+
+			position: fixed;
+			bottom: 0;
+
+			width: 100%;
+			height: $height;
+
+			display: flex;
+
+			background: #fff;
+
+			text-align: center;
+			line-height: $height;
+			font-size: $font-size-28;
+
+			.custom {
+				width: upx(300);
+			}
+
+			.buy{
+				width: upx(450);
+				background: #000;
+				color: #fff;
+			}
 		}
 	}
 </style>
