306ef8f89598cbf1fc1cdf5fe3983d701632579f
bevexed <547551650@qq.com>
2019-06-06 16:49:45 +0800

    今日 收益

--- src/pages/today-come/today-come.vue
+++ src/pages/today-come/today-come.vue
@@ -1,9 +1,5 @@
 <template>
 	<div>
-		<header>
-			<div>累计收益：28600.00</div>
-			<div class="sm">待结算：800.00</div>
-		</header>
 		<my-list></my-list>
 	</div>
 </template>
@@ -19,24 +15,3 @@
 		}
 	})
 </script>
-
-<style lang="scss" scoped>
-	@import "../../uni";
-
-	header {
-		box-sizing: border-box;
-		width: upx(710);
-		margin: upx(20) auto;
-		padding: upx(24);
-		background: #000;
-		border-radius: upx(10);
-		color: #fff;
-		text-align: center;
-		font-size: $font-size-28;
-
-		.sm {
-			font-size: $font-size-20;
-			font-weight: lighter;
-		}
-	}
-</style>
